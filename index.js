6b5b33ed8fe20725df29d5698c8d89594693ff0d
Jake Russo <madcap.russo@gmail.com>
2015-07-24 12:57:35 -0500

    Change to lowercase name for eslint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const HTMLHint = require("htmlhint").HTMLHint
+const htmlhint = require("htmlhint").HTMLHint
 
 module.exports = function () {
   this.filter("htmlhint", (source, options) => {
     try {
-      const messages = HTMLHint(source, options)
+      const messages = htmlhint(source, options)
       console.log(messages)
       return source
      } catch (e) { throw e }
