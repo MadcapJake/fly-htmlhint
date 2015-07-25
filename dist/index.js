31714443eeb4bf46414378913bd1e667ccca1eeb
Jake Russo <madcap.russo@gmail.com>
2015-07-24 19:18:49 -0500

    Add pass func but left unused til fly-util PR

--- dist/index.js
+++ dist/index.js
@@ -3,20 +3,25 @@
 var htmlhint = require("htmlhint").HTMLHint;
 var clor = require("clor");
 var assign = require("object-assign");
+// const util = require("fly-util")
 
 var fmt = assign(clor, {
-  line: clor.bold.blue("%d"),
-  col: clor.bold.green("%d"),
-  error: clor.bold.red("%s"),
-  warn: clor.bold.yellow("%s"),
-  debug: clor.bold.white("%s"),
-  rule: clor.underline.cyan("%s")
+  pos: clor.underline.cyan("%d:%d"),
+  message: clor.bold.white("%s"),
+  rule: clor.bold.blue("%s")
 });
 
 module.exports = function () {
   var _this = this;
 
-  this.filter("htmlhint", function (source, options) {
+  /*eslint-disable no-unused-vars*/
+  this.pass = function () {}
+  // TODO: needs stamp exported to work
+  // util.stamp.apply({ method: "log", color: "green" }, args)
+
+  /*eslint-enable*/
+
+  ;this.filter("htmlhint", function (source, options) {
     var counts = {
       errors: 0,
       warnings: 0
@@ -25,19 +30,21 @@ module.exports = function () {
       htmlhint.verify(source, options).forEach((function (msg) {
         if (msg.type === "error") {
           counts.errors++;
-          this.error(fmt.line + " : " + fmt.col + " " + fmt.error + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
+          this.error("HTMLHint: " + fmt.pos + " " + fmt.message + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
         } else if (msg.type === "warning") {
           counts.warnings++;
-          this.warn(fmt.line + " : " + fmt.col + " " + fmt.warn + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
+          this.warn("HTMLHint: " + fmt.pos + " " + fmt.message + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
         } else if (msg.type === "info") {
-          this.debug(fmt.line + " : " + fmt.col + " " + fmt.debug + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
+          this.debug("HTMLHint: " + fmt.pos + " " + fmt.message + " " + fmt.rule, msg.line, msg.col, msg.message, msg.rule.id);
         }
       }).bind(_this));
     } catch (e) {
       throw e;
     }
     var total = counts.errors + counts.warnings;
-    _this.error("%s %d problem%s (%d error%s, %d warning%s)", total >= 1 ? "✖" : "✓", total, total > 1 || total === 0 ? "s" : "", counts.errors, counts.errors > 1 || counts.errors === 0 ? "s" : "", counts.warnings, counts.warnings > 1 || counts.warnings === 0 ? "s" : "");
+    // TODO: change `log` to `pass` once stamp PR is merged
+    var success = total >= 1 ? "error" : "log";
+    _this[success]("HTMLHint: %s %d problem%s (%d error%s, %d warning%s)", total >= 1 ? "✖" : "✓", total, total > 1 || total === 0 ? "s" : "", counts.errors, counts.errors > 1 || counts.errors === 0 ? "s" : "", counts.warnings, counts.warnings > 1 || counts.warnings === 0 ? "s" : "");
     return source;
   }, { ext: ".html" });
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const htmlhint = require("htmlhint").HTMLHint
 const clor = require("clor")
 const assign = require("object-assign")
+// const util = require("fly-util")
 
 const fmt = assign(clor, {
-  line: clor.bold.blue("%d"),
-  col: clor.bold.green("%d"),
-  error: clor.bold.red("%s"),
-  warn: clor.bold.yellow("%s"),
-  debug: clor.bold.white("%s"),
-  rule: clor.underline.cyan("%s")
+  pos: clor.underline.cyan("%d:%d"),
+  message: clor.bold.white("%s"),
+  rule: clor.bold.blue("%s")
 })
 
 module.exports = function () {
+
+  /*eslint-disable no-unused-vars*/
+  this.pass = function (...args) {
+    // TODO: needs stamp exported to work
+    // util.stamp.apply({ method: "log", color: "green" }, args)
+  }
+  /*eslint-enable*/
+
+
   this.filter("htmlhint", (source, options) => {
     let counts = {
       errors: 0,
@@ -21,22 +28,27 @@ module.exports = function () {
       htmlhint.verify(source, options).forEach(function (msg) {
         if (msg.type === "error") {
           counts.errors++
-          this.error(`${fmt.line} : ${fmt.col} ${fmt.error} ${fmt.rule}`,
-            msg.line, msg.col, msg.message, msg.rule.id)
+          this.error(`HTMLHint: ${fmt.pos} ${fmt.message} ${fmt.rule}`,
+            msg.line, msg.col,
+            msg.message, msg.rule.id)
         }
         else if (msg.type === "warning") {
           counts.warnings++
-          this.warn(`${fmt.line} : ${fmt.col} ${fmt.warn} ${fmt.rule}`,
-            msg.line, msg.col, msg.message, msg.rule.id)
+          this.warn(`HTMLHint: ${fmt.pos} ${fmt.message} ${fmt.rule}`,
+            msg.line, msg.col,
+            msg.message, msg.rule.id)
         }
         else if (msg.type === "info") {
-          this.debug(`${fmt.line} : ${fmt.col} ${fmt.debug} ${fmt.rule}`,
-            msg.line, msg.col, msg.message, msg.rule.id)
+          this.debug(`HTMLHint: ${fmt.pos} ${fmt.message} ${fmt.rule}`,
+            msg.line, msg.col,
+            msg.message, msg.rule.id)
         }
       }.bind(this))
     } catch (e) { throw e }
     const total = counts.errors + counts.warnings
-    this.error(`%s %d problem%s (%d error%s, %d warning%s)`,
+    // TODO: change `log` to `pass` once stamp PR is merged
+    const success = total >= 1 ? "error" : "log"
+    this[success](`HTMLHint: %s %d problem%s (%d error%s, %d warning%s)`,
       total >= 1 ? "✖" : "✓",
       total, total > 1 || total === 0 ? "s" : "",
       counts.errors, counts.errors > 1 || counts.errors === 0 ? "s" : "",
